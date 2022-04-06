f6c930daf07cdaeef879156a07de0ca65d5e4559
Eric Rajaonarison <eric.raj12@gmail.com>
2022-04-07 02:02:24 +0300

    Map

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import mapboxgl from "mapbox-gl";
+import React from "react";
+import Map, { NavigationControl, ViewportProps } from "react-map-gl";
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+(mapboxgl as any).workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 function App() {
+  const initialViewport: ViewportProps = {
+    latitude: -18.88281,
+    longitude: 47.520817,
+    zoom: 14
+  };
+
+  const [viewport, setViewport] = React.useState<ViewportProps>(initialViewport);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Demo react-map-gl-v6</h1>
+      <div className="MapContainer">
+        <Map
+          {...viewport}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          attributionControl={true}
+          onViewportChange={(nextViewport: ViewportProps) => setViewport(nextViewport)}
         >
-          Learn React
-        </a>
-      </header>
+          <NavigationControl className="MapNavigationControl" />
+        </Map>
+      </div>
     </div>
   );
 }
