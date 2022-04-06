682a6c63f5edbc30fbfc77c020cdaca0a40a1fda
Eric Rajaonarison <eric.raj12@gmail.com>
2022-04-07 02:02:24 +0300

    Map with react-map-gl v7

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import mapboxgl from "mapbox-gl";
+import React from "react";
+import Map, { NavigationControl, ViewState } from "react-map-gl";
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+(mapboxgl as any).workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+
 
 function App() {
+  const initialViewState: Partial<ViewState> = {
+    latitude: -18.88281,
+    longitude: 47.520817,
+    zoom: 14
+  };
+
+  const [viewState] = React.useState<Partial<ViewState>>(initialViewState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Demo react-map-gl-v7</h1>
+      <div className="MapContainer">
+        <Map
+          initialViewState={{ ...viewState }}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         >
-          Learn React
-        </a>
-      </header>
+          <NavigationControl position="top-right" />
+        </Map>
+      </div>
     </div>
   );
 }
